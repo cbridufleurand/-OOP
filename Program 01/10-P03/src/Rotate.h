447be9a5afa1226d502d2f921e3db9022e1be17e
cbridufleurand <cbdufleurand0109@my.msutexas.edu>
2026-05-12 20:38:06 -0500

    Update Rotate.h

--- Program 01/10-P03/src/Rotate.h
+++ Program 01/10-P03/src/Rotate.h
@@ -1,1 +1,18 @@
+#ifndef ROTATE_H
+#define ROTATE_H
 
+#include "Filter.h"
+
+class Rotate : public Filter {
+private:
+    int degrees;
+
+public:
+    Rotate(int deg);
+
+    void apply(Grid& pixels) override;
+
+    std::string name() const override;
+};
+
+#endif
